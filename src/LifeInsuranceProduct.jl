316f730771282d37dccd5ff6e5e3628dc4063cd6
Michael Fliegner <Michael.Fliegner@hamburg.de>
2022-08-21 09:12:53 +0200

    annuities working

--- src/LifeInsuranceProduct.jl
+++ src/LifeInsuranceProduct.jl
@@ -4,7 +4,7 @@ using Dates, LifeContingencies
 using MortalityTables
 using Yields
 import LifeContingencies: V, ä     # pull the shortform notation into scope
-export calculate
+export calculate!
 
 # load mortality rates from MortalityTables.jl
 vbt2001 = MortalityTables.table("2001 VBT Residual Standard Select and Ultimate - Male Nonsmoker, ANB")
@@ -17,7 +17,7 @@ function insurance_age(dob, beginn)::Integer
     end
 end
 
-function calculate(ti)
+function calculate!(ti)
     dob = ti.partner_refs[1].ref.revision.date_of_birth
     beginn = Date(Dates.year(today()), Dates.month(today()) + 1, 1)
     """
@@ -31,7 +31,7 @@ function calculate(ti)
         mortality=vbt2001.select[issue_age],    # -- Mortality rates
     )
 
-    yield = Yields.Constant(0.05)      # Using a flat 5% interest rate
+    yield = Yields.Constant(0.0125)      # Using a flat 1,25% interest rate
 
     lc = LifeContingency(life, yield)  # LifeContingency joins the risk with interest
 
@@ -40,6 +40,16 @@ function calculate(ti)
     ins = Insurance(life, yield)       # alternate way to construct
 
     premium_net(lc)
+
+    ai = AnnuityImmediate(lc)
+    if (ti.tariff_ref.rev.deferment == 0.0)
+        ti.tariff_ref.rev.annuity_due = 0.0
+        ti.tariff_ref.rev.annuity_immediate = present_value(AnnuityImmediate(lc))
+    else
+        ti.tariff_ref.rev.annuity_immediate = 0.0
+        ti.tariff_ref.rev.annuity_due = present_value(AnnuityDue(lc, start_time=ti.tariff_ref.rev.deferment))
+    end
+    ti
 end
 
 end # module
